e19b342579471fa4cca98f94c32dcd621696a6d4
draco <122390064+Darco786@users.noreply.github.com>
2023-03-22 02:26:24 +0530

    new

--- src/App.js
+++ src/App.js
@@ -4,19 +4,34 @@ import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 import Nft from './components/Nft/Nft';
 import Mobfooter from './components/Mobfooter/Mobfooter';
+import React, { useEffect, useState } from 'react'
+import Loadar from './components/Loader/Loadar';
 
 function App() {
+  const [loading,setLoading] =useState(false)
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },5000)
+  },[])
   return (
-    <>
+    <div>
+     {
+      loading ?
+     <Loadar/>
+      :
+      <div>
     <div className='main-bg' >
        <img src={Light} alt="" className="lig-4"/>
       <Navbar/>
       <Hero/>
       <Nft/>
-   
     </div>
     <Mobfooter/>
-    </>
+    </div>
+     }
+    </div>
     
   );
 }
